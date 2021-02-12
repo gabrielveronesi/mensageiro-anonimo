325b3afe61a337baa7d41ad15e7a5956b016f0a1
Gabriel Veronesi <veronesigabriel@live.com>
2021-02-12 18:18:33 -0300

    BodyParse

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const nodemailer = require("nodemailer");
 const SMTP_CONFIG = require("./src/config/smtp");
 const express = require("./src/config/customexpress")
 const app = express();
+const bodyParser = require('body-parser')
+
 //const PORT = process.env.PORT || 3000;
    /* 
     app.listen(PORT, () => {
       console.log('Servidor rodando na porta 3000')
     })
 */
+
+//Body Parser
+ app.use(bodyParser.urlencoded({extended:false}))
+ app.use(bodyParser.json())
 
 app.listen(process.env.PORT || 3000);
 
@@ -24,13 +30,13 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-
-
-
 app.post('/enviaremail', (req, res) => {
+
   function eenviarEmail() {
+    console.log(req.body.email);
+    console.log(req.body.email);
     const mailSent = transporter.sendMail({
-      text: "email", //texto
+      text: "emailll", //texto
       subject: "Assunto do e-mail", 
       from: "msga-enviamensagem@outlook.com", //quem vai enviar?
       to: ["msga-recebermensagem@outlook.com"],//quem vai receber?
@@ -39,13 +45,23 @@ app.post('/enviaremail', (req, res) => {
   eenviarEmail();
  })
 
+ https://www.youtube.com/watch?v=P1OI_EKyl6U&list=PLJ_KhUnlXUPtbtLwaxxUxHqvcNQndmI4B&index=22
 
-app.get('/exibealerta', (req, res) => {
-  function exibealerta(){
-    alert("Pegar doc funcio.js");
-    }
-    exibealerta();
+ /*
+
+app.post('/enviaremail', (req, res) => {
+  function eenviarEmail() {
+    const mailSent = transporter.sendMail({
+      text: emaill, //texto
+      subject: "Assunto do e-mail", 
+      from: "msga-enviamensagem@outlook.com", //quem vai enviar?
+      to: ["msga-recebermensagem@outlook.com"],//quem vai receber?
+    });
+  }
+  eenviarEmail();
  })
+
+*/
 
  app.get('/', (req, res) => {
   res.render("index")
@@ -65,3 +81,4 @@ app.get('/exibealerta', (req, res) => {
 
 
  app.set('view engine', 'ejs');
+
